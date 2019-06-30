47d7d7b46e127fc27a39fc4a96b5f269c65fce8e
cyclingscotty <46350366+cyclingscotty@users.noreply.github.com>
2019-06-30 23:09:30 +0800

    Add files via upload

--- Unit1/Lesson1/app.js
+++ Unit1/Lesson1/app.js
@@ -2,7 +2,7 @@ let myData = {
     up: {
         "vocab": "up the street",
         "explanation": "'Up' can be indicate the destination is higher OR it can be used to indicate the destination is more northerly.",
-        "example": "E.g. To go to school, I need to walk <span class='wordUse'>up</span> a steep hill",
+        "example": "E.g. To go to school, I need to walk <span class='wordUse'>up</span> a steep hill.",
         "image": "https://dynaimage.cdn.cnn.com/cnn/q_auto,w_900,c_fill,g_auto,h_506,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F140304172635-worlds-steepest-street-runner.jpg"
     },
     over: {
