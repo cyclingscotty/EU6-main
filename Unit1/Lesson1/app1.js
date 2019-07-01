7ee13a342dd407823a535a7f000426d3629ed609
cyclingscotty <46350366+cyclingscotty@users.noreply.github.com>
2019-07-01 15:34:45 +0800

    Add files via upload

---
+++ Unit1/Lesson1/app1.js
@@ -0,0 +1,31 @@
+const arr = [
+    "Walk up the street and turn right.",
+    "Walk down the stairs and turn right.",
+    "Walk around the fountain and go straight.",
+    "Walk over the bridge and turn left.",
+    "Go under the train tracks and go straight.",
+    "Walk along the river and turn left."
+]
+
+document.querySelector('body').addEventListener('click', function(){
+ if(event.target.className === 'activityPics'){
+    let pressedButtonNumber = event.target.id.slice(8,9);
+    checkAnswer(pressedButtonNumber);
+ }
+});
+
+function checkAnswer(x){
+    let inputted = document.querySelector('#answer'+x).value;
+    let stored = arr[x-1];
+    console.log(inputted);
+    console.log(stored);
+    
+    if( inputted == stored){
+        document.getElementById('activity'+x+'QandABox').style.backgroundColor = '#bef5bc';
+    } else {
+        document.getElementById('activity'+x+'QandABox').style.backgroundColor = '#ef8080';
+        setTimeout(function(){
+            document.getElementById('activity'+x+'QandABox').style.backgroundColor = '#ffffff'}, 1000);
+     
+           }
+}
