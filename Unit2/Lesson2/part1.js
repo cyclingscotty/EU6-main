1c8e61adaf2f7ef545fe2f2e3c8405352dcda756
cyclingscotty <46350366+cyclingscotty@users.noreply.github.com>
2019-07-23 16:36:03 +0800

    Add files via upload

--- Unit2/Lesson2/part1.js
+++ Unit2/Lesson2/part1.js
@@ -2,19 +2,31 @@ let theVocabs = [
     'call friends', 'stay up late', 'invite a friend over', 'watch a scary movie', 'sing karaoke', 'have a party'
 ];
 
+let theAltVocabs = [
+    'invite over a friend'
+]
+
+
+    let howManyVocabs = [];
+    howManyVocabs = document.getElementsByClassName('vocab');
+
 function vocabSlots(){
-    for (let i = 0; i < howManyVocabs.length; i++){
+    for (let i = 0; i < theVocabs.length; i++){    
     howManyVocabs[i].innerHTML = theVocabs[i];
 }
+    document.getElementById('start1').addEventListener('click', clearVocabSlots);
 }
 
 function clearVocabSlots(){
+    
     for (let i = 0; i < howManyVocabs.length; i++){
     howManyVocabs[i].innerHTML = "<input type='text' size='20'><button>check</button>";
 }
+
 }
 
-let howManyVocabs = document.getElementsByClassName('vocab');
+
+
 vocabSlots();
 
 
